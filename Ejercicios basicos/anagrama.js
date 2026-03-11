e170d0adf0950e8f32839e94543fe17fbe04d8e5
Alex Urquiza <alex.urz20@gmail.com>
2026-03-11 16:38:08 -0300

    Actualizacion de anagrama.js

--- Ejercicios basicos/anagrama.js
+++ Ejercicios basicos/anagrama.js
@@ -1,14 +1,34 @@
 
+// function esAnagrama(p1, p2){
+//     if(p1 === p2) return false;
+//     if(p1.length !== p2.length) return false;
+
+//     let ordenoP1 = p1.split("").sort().join("");
+//     let ordenoP2 = p2.split("").sort().join("");
+    
+//     return ordenoP1 === ordenoP2 ? true : false;   
+// }
+
+
+//Usando conteo de frecuencia
 function esAnagrama(p1, p2){
     if(p1 === p2) return false;
     if(p1.length !== p2.length) return false;
+    let contador = {};
 
-    let ordenoP1 = p1.split("").sort().join("");
-    let ordenoP2 = p2.split("").sort().join("");
-    
-    return ordenoP1 === ordenoP2 ? true : false;   
+    for(let letra of p1){
+        contador[letra] = ((contador[letra] || 0) + 1);
+    }
+
+    for(let letra2 of p2){
+        if(!contador[letra2]) return false;
+        contador[letra2]--;
+    }
+
+    return true;
 }
 
+
 console.log(esAnagrama("amor", "roma"));
-console.log(esAnagrama("rota", "otra"));
-console.log(esAnagrama("otra", "otra"));
+// console.log(esAnagrama("rota", "otra"));
+// console.log(esAnagrama("otra", "otra"));
